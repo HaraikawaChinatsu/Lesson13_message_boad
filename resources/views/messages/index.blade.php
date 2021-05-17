7e0c13e09132de00d3c11fc2920853d0b2cd2157
chinatsu.haraikawa <pechi72@icloud.com>
2021-05-17 08:53:40 +0000

    complate

--- resources/views/messages/index.blade.php
+++ resources/views/messages/index.blade.php
@@ -16,10 +16,6 @@
             <tbody>
                 @foreach ($messages as $message)
                 <tr>
-                    <td>{{ $message->id }}</td>
-                    <td>{{ $message->content }}</td>
-                </tr>
-                <tr>
                     {{-- メッセージ詳細ページへのリンク --}}
                     <td>{!! link_to_route('messages.show', $message->id, ['message' => $message->id]) !!}</td>
                     <td>{{ $message->content }}</td>
